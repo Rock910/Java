255312bc0b84a3f2119e439c6cc0eed3af5c7eab
Joshua Steier <josteier@optonline.net>
2018-01-03 12:21:03 -0500

    Create LinearSearch2.java

---
+++ LinearSearch2.java
@@ -0,0 +1,39 @@
+package com.company;
+//Demonstration of linear search(my version)
+//import scanner and arrays
+import java.util.Arrays;
+import java.util.Scanner;
+public class LinearSearch2 {
+    public static void main(String[] args) {
+        //initalize scanner, size and search key
+        Scanner input = new Scanner(System.in);
+        int size;
+        int searchKey;
+        //Prompt user for input
+        System.out.print("Enter in a size value: ");
+        size = input.nextInt();
+        System.out.print("Enter in a search key: ");
+        searchKey = input.nextInt();
+        //Declare an array of size: prompt for by user
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            //Add values to the array
+            System.out.print("Enter in a value to the array: ");
+            array[i] = input.nextInt();//Implement the linear search
+        }
+        for (int index = 0; index < size; index++) {
+            //search through the array
+            if (array[index] == searchKey) {
+                //need to return index
+                System.out.printf("Index is %d\n", index);
+
+            }
+        }
+        System.out.printf("The array has values: %s\n", Arrays.toString(array));
+        System.out.printf("Search key returned is: %d\n", searchKey);
+
+    }
+
+
+    }
+

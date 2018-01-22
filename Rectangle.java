efce15848c0ceba9244f3b6933e673f633fb4b19
Joshua Steier <josteier@optonline.net>
2018-01-22 10:04:35 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -21,23 +21,36 @@ public class Rectangle {
 
 
     }
+    //REQUIRES: height to be of type int
+    //MODIFIES: this
+    //EFFECTS: sets Height value to be height entered by user.
     public void setHeight(int height){
         Height= height;
     }
+    //REQUIRES: width be of type int
+    //MODIFIES: this
+    //EFFECTS: sets Width to value width entered by user.
     public void setWidth(int width){
         Width= width;
     }
+    //REQUIRES: height and width be of type int
+    //MODIFIES: this
+    //EFFECTS: returns perimeter value, given height and width
     public int getPerimeter(int height, int width){
         Height= height;
         Width= width;
         return Height + Width;
     }
+    //REQUIRES: height, width of type int
+    //EFFECTS: returns area given int width and int height
     public int getArea(int height, int width){
         return Height * Width;
     }
+    //EFFECTS: returns height value
     public int getHeight(){
         return Height;
     }
+    //EFFECTS: returns width value
     public int getWidth(){
         return Width;
     }
